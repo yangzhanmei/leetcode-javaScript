4e1fcdf9740a9c4d33b8d67edfcf0394d000c5d0
yangzhanmei <15809244435@163.com>
2019-03-27 08:45:08 +0800

    442_Find All Duplicates in an Array

---
+++ src/442_Find All Duplicates in an Array/index.js
@@ -0,0 +1,20 @@
+/**
+ * nums item as index to mark 
+ * if marked then push to result
+ */
+const findDuplicates = function(nums) {
+    if(nums.length === 0) {
+        return [];
+    }
+    const result = [];
+    for(let i = 0; i < nums.length; i++) {
+        const index = Math.abs(nums[i])-1;
+        if(nums[index] < 0) {
+            result.push(index+1);
+        }else {
+           nums[index] = -nums[index] 
+        }
+    }
+    
+    return result;
+};
