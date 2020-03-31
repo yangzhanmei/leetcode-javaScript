c8cae84f31dc4dfab8a604dd695365eddc12a493
yangzhanmei <15809244435@163.com>
2020-03-31 22:17:11 +0800

    feat: 912 quick sort

--- src/912_Sort an Array/index.js
+++ src/912_Sort an Array/index.js
@@ -2,6 +2,37 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// var sortArray = function(nums) {
+//   return nums.sort((a, b) => a - b);
+// };
+
 var sortArray = function(nums) {
-  return nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+
+  return quickSort(nums, left, right);
 };
+
+function quickSort(nums, left, right) {
+  let i = left;
+  let j = right;
+  let num = nums[i];
+  if (i < j) {
+    while (i < j) {
+      while (nums[j] >= num && i < j) {
+        j--;
+      }
+      nums[i] = nums[j];
+      while (nums[i] <= num && i < j) {
+        i++;
+      }
+      nums[j] = nums[i];
+    }
+    nums[i] = num;
+    quickSort(nums, left, i - 1);
+    quickSort(nums, i + 1, right);
+  }
+
+  return nums;
+}
