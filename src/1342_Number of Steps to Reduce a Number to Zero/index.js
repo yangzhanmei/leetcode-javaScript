ed87617f83ad2e1f6671afdc4db1531e99247e07
yangzhanmei <15809244435@163.com>
2020-04-10 22:48:16 +0800

    feat: add 1342

---
+++ src/1342_Number of Steps to Reduce a Number to Zero/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+  let result = 0;
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num = num / 2;
+    } else {
+      num -= 1;
+    }
+    result += 1;
+  }
+  return result;
+};
